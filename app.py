7428a9d4dceacb1c2638a5d6744c412e2b1f3a5d
Soobin-329 <81364415+Soobin-329@users.noreply.github.com>
2022-08-15 13:43:38 +0900

    add stopwords

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ def keyword_extract(sentences):
                  '다음주', '별일', '일도', '일이', '크게', '방금', '갑자기', '계속', '저번', '심지어', '위해', '꿈속', '끼리',
                  '억지로', '그것', '어제', '일단', '타고', '마침', '사려', '못', '일', '무슨', '찌', '여', '아예', '그냥',
                  '거기', '거의', '바로', '존나', '인지', '역시', '조금', '자꾸', '내야', '하니', '약간', '본격', '달라',
-                 '정말', '얼마']
+                 '정말', '얼마', '가장', '동안', '보고', '보기', '아무', '가운데', '앞', '뒤', '중간', '밑', '위', '아래',
+                 '옆', '대신', '마냥', '다해', '다행', '화가', '강제']
 
     tokenized_doc = okt.pos(sentences, stem=True)
     array_text = [word[0] for word in tokenized_doc if word[1] == 'Noun' and word[0] not in stopwords]
@@ -33,7 +34,7 @@ def keyword_extract(sentences):
 
     keyword = pd.DataFrame(new_bow, columns=['keyword', 'weight'])
     result = keyword.groupby('keyword').agg('sum').sort_values('weight', ascending=False).head(20)
-    return list(result.index)[:5]  # 키워드 수 조절 가능
+    return list(result.index)[:3]  # 키워드 수 조절 가능
 
 
 # flask
